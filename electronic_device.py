fb1ab7aa4a17aa1335cb39177bcf81acd500adee
RogerHagman <rogerhagman84@gmail.com>
2023-09-28 13:45:26 +0200

    0.03 added graphical user interface

--- electronic_device.py
+++ electronic_device.py
@@ -5,14 +5,43 @@ from abc import ABC, abstractmethod
 
 class ElectronicDevice(ABC):
     
+    def __init__(self, max_battery_life: int, battery_life: int = None):
+        self._max_battery_life = battery_life
+        self._battery_life = battery_life if battery_life is not None \
+            else max_battery_life
+
+    @property
+    def max_battery_life(self):
+        return self._max_battery_life
+
+    @max_battery_life.setter
+    def battery_life(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Max Battery life must be an integer")
+        if value < 0:
+            raise ValueError("Max Battery life cannot be negative")
+        self._battery_life = value
+
+    @property
+    def battery_life(self):
+        return self._battery_life
+
+    @battery_life.setter
+    def battery_life(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Battery life must be an integer")
+        if value < 0:
+            raise ValueError("Battery life cannot be negative")
+        self._battery_life = value
+
     @abstractmethod
-    def get_battery_life(self, battery_life):
+    def get_battery_life(self):
         pass
 
 class SmartPhone(ElectronicDevice):
 
     def __init__(self):
-        self._battery_life = 10
+        super().__init__(max_battery_life = 10, battery_life = 10)
 
     def get_battery_life(self, battery_life = None):
         if battery_life is not None:
@@ -22,7 +51,7 @@ class SmartPhone(ElectronicDevice):
 class Laptop(ElectronicDevice):
 
     def __init__(self):
-        self._battery_life = 5
+        super().__init__(max_battery_life = 5, battery_life = 5)
 
     def get_battery_life(self, battery_life = None):
         if battery_life is not None:
@@ -32,7 +61,7 @@ class Laptop(ElectronicDevice):
 class SmartWatch(ElectronicDevice):
 
     def __init__(self):
-        self._battery_life = 24
+        super().__init__(max_battery_life = 24, battery_life = 24)
 
     def get_battery_life(self, battery_life = None):
         if battery_life is not None:
--- main.py
+++ main.py
@@ -1,27 +1,87 @@
 """Bat Signal The Battery Reader"""
-
 from __future__ import annotations
 from abc import ABC, abstractmethod
+import tkinter as tk
+from tkinter import ttk
 from electronic_device import ElectronicDevice
 from electronic_device import SmartPhone
 from electronic_device import Laptop
 from electronic_device import SmartWatch
 
-VERSION = 0.02
+VERSION = 0.03
 
 def display_battery_life(device:ElectronicDevice):
     print(device.get_battery_life())
 
+class BatteryWidget(ttk.Frame):
+    def __init__(self, parent, device, **kwargs):
+        super().__init__(parent, **kwargs)
+
+        self.device = device
+        self.grid(sticky=(tk.W + tk.E + tk.N + tk.S))
+
+        self.battery_label = ttk.Label(self, text=self.device.get_battery_life())
+        self.battery_label.grid(row=0, column=0, columnspan=3, pady=5)
+
+        self.canvas = tk.Canvas(self, width=100, height=30, bg='white')
+        self.canvas.grid(row=1, column=0, columnspan=3, pady=10)
+
+        self.increase_button = ttk.Button(self, text="+1", 
+                                          command=self.increase_battery)
+        self.increase_button.grid(row=2, column=1, sticky=tk.W+tk.E)
+
+        self.decrease_button = ttk.Button(self, text="-1", 
+                                          command=self.decrease_battery)
+        self.decrease_button.grid(row=2, column=0, sticky=tk.W+tk.E)
+
+        self.charge_button = ttk.Button(self, text="Charge",
+                                        command=self.charge_battery)
+        self.charge_button.grid(row=2, column=2, sticky=tk.W+tk.E)
+
+        self.update_battery_graphics()
+
+    def increase_battery(self):
+        self.device.battery_life = min(self.device.battery_life + 1, self.device.max_battery_life)
+        self.update_display()
+
+    def decrease_battery(self):
+        self.device.battery_life = max(self.device.battery_life - 1, 0)
+        self.update_display()
+
+    def charge_battery(self):
+        self.device.battery_life = self.device.max_battery_life
+        self.update_display()
+
+    def update_display(self):
+        self.battery_label["text"] = self.device.get_battery_life()
+        self.update_battery_graphics()
+
+    def update_battery_graphics(self):
+        self.canvas.delete("all")
+        fill_percent = self.device.battery_life / self.device.max_battery_life
+        fill_width = int(fill_percent * 100)
+        if 0 < fill_percent <= 0.20:
+            self.canvas.create_rectangle(0, 0, fill_width, 30, fill='red')
+        elif .20 < fill_percent < 0.70:
+            self.canvas.create_rectangle(0, 0, fill_width, 30, fill='yellow')
+        else:
+            self.canvas.create_rectangle(0, 0, fill_width, 30, fill='green')
+
 def main():
+    root = tk.Tk()
+    root.geometry("250x400")
+    root.title("Bat Signal: The Battery Reader")
 
-    devices = {
-    "smartphone": SmartPhone(),
-    "laptop": Laptop(),
-    "smartwatch": SmartWatch()
-    }
-    
-    for device in devices:
-        display_battery_life(devices.get(device))
+    smartphone = SmartPhone()
+    laptop = Laptop()
+    smartwatch = SmartWatch()
 
-if __name__ == '__main__':
+    devices = [smartphone, laptop, smartwatch]
+    for idx, device in enumerate(devices):
+        widget = BatteryWidget(root, device)
+        widget.grid(row=idx, pady=10)
+        widget.update_battery_graphics()
+    root.mainloop()
+
+if __name__ == "__main__":
     main()
